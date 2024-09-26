23d222075659c00475235ed5d0e1e3c63c0313f8
ana maria <anamaria62003@gmail.com>
2024-09-26 16:46:44 -0500

    esta repositorio es para hacer ejemplos de los operadores

---
+++ operadores_ejemplo/operacionesComparacion.py
@@ -0,0 +1,10 @@
+#ejemplo de operaciones de comparacion
+m = int(input("ingrese el valor de m:"))
+n= int(input("ingrese el valor de n:"))
+
+print(f"m y n son iguales? {m==n}")# igualdad
+print(f"m es mayor que n?:{m > n}")# mayor que >
+print(m < n)# menor que <
+print(m >= n)# mayor igual que >=
+print(m <= n)# menor igual que <=
+print(m != n)# no igual !
---
+++ operadores_ejemplo/precedenciaOperadores.py
@@ -0,0 +1,12 @@
+#ejemplo de una expresion 42//6+7*3-39
+
+resultado0 =42//6+7*3-39
+resultado1 = (42//6)+7*-39
+resultado2= (42//6) +(7*3)-39
+resultado3 = ((42//6)+(7*3))-39
+resultado4 =( ((42//6)+(7*3))-39)
+
+
+print(resultado0)
+print(resultado2)
+print(resultado3)
